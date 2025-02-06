80b73d0f2aa70340a17a1ee6addf60d763cc00a2
untitlez <tleza_championship2@hotmail.com>
2025-02-07 01:09:34 +0700

    update

---
+++ app/page/cart/page.js
@@ -0,0 +1,13 @@
+export default function CartPage(){
+  
+    return (
+      <>
+        <div className="fill">
+          <h2>🛒 Cart</h2>
+          <div className=" flex flex-wrap justify-evenly gap-10 my-8">
+          </div>
+          Empty
+        </div>
+      </>
+    )
+  }
---
+++ app/page/login/page.js
@@ -0,0 +1,11 @@
+import Login from "@/app/components/form/Login";
+
+export default function LoginPage() {
+  return (
+    <>
+      <div className="fill h-screen">
+        <Login/>        
+      </div>
+    </>
+  );
+}
