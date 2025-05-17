04309c7fde3668a1afdf04394122f85efa159bbf
untitlez <tleza_championship2@hotmail.com>
2025-05-17 14:16:21 +0700

    edit fetch api

--- app/(pages)/admin/page.js
+++ app/(pages)/admin/page.js
@@ -1,5 +1,4 @@
 import Link from "next/link";
-import axios from "axios";
 import { API_URL } from "@/app/lib/constant/admin/api";
 
 import { StatUsers } from "@/app/components/admin/StatUsers";
@@ -7,14 +6,15 @@ import { TableUsers } from "@/app/components/admin/TableUsers";
 import { ToggleTheme } from "@/app/components/ToggleTheme";
 
 export default async function AdminPage() {
-  const { data } = await axios.get(API_URL);
+  const res = await fetch(API_URL, { cache: "no-store" });
+  const data = await res.json();
 
   return (
     <>
       <section className="flex justify-between items-center">
         <h1 className="font-semibold lg:text-2xl">Dash Board</h1>
         <div className="flex items-center gap-2">
-          <ToggleTheme id="admin" main="luxury" sub="winter"/>
+          <ToggleTheme id="admin" main="luxury" sub="winter" />
           <Link
             href="/admin/add-user"
             className="btn btn-sm lg:btn text-xs lg:text-base"
