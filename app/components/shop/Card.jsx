5cfe3ab427224ea2b280e0373cd67e5e00b8f8b3
untitlez <tleza_championship2@hotmail.com>
2026-02-05 21:28:44 +0700

    fix : params id

--- app/components/shop/Card.jsx
+++ app/components/shop/Card.jsx
@@ -1,13 +1,9 @@
 "use client";
 
-import { useRouter } from "next/navigation";
 import { useShop } from "@/app/lib/store/shop";
 
 export const Card = ({ data }) => {
-  const router = useRouter();
   const { setCart } = useShop();
-
-  const seeMore = (id) => router.push(`/shop/${id}`);
   const handleAdd = (item) => setCart(item);
 
   return (
@@ -19,11 +15,7 @@ export const Card = ({ data }) => {
           text-sm lg:text-base sm:hover:-translate-y-3 duration-300"
         >
           <figure className="sm:aspect-square grid place-content-center bg-[#EDEBE9] shadow rounded-lg p-2 ">
-            <img
-              src={item.image}
-              alt={item.name}
-              onClick={() => seeMore(item.id)}
-            />
+            <img src={item.image} alt={item.name} />
           </figure>
           <div className="flex flex-col justify-between h-full w-full">
             <p className="font-medium">{item.name}</p>
